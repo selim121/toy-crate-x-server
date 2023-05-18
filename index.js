9c23f64bdb0a7e163cf4c3660c17ed60bb11c3af
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-18 17:48:50 +0600

    users post added

--- index.js
+++ index.js
@@ -26,13 +26,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const usersCollection = client.db("toyCrateX").collection("users");
+
+    app.post('/users', async(req, res) => {
+        const body = req.body;
+        const result = await usersCollection.insertOne(body);
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
